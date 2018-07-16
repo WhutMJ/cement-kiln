4de2d9d18e0ff100738da2750637bc6df6527fe2
WhutMJ <30967518+WhutMJ@users.noreply.github.com>
2018-07-16 09:37:22 +0800

    Add files via upload

---
+++ provide_data_for_gui.py
@@ -0,0 +1,52 @@
+import xlrd
+
+
+def get_by_hour(time):          #获取具体一小时的横向数据
+    '''
+    :param time: 输入的时间
+    :return: 两个数值，变量名和对应数值
+    '''
+    filename = '三线窑操作记录总表.xlsx'
+    readfile = xlrd.open_workbook(filename)
+    read_sheet = readfile.sheet_by_name('Sheet1')
+    table_data = read_sheet.col_values(0, 1)
+    index = 0
+    for data in table_data:
+        if data == time:
+            index = table_data.index(data)
+            break
+    table_value = read_sheet.row_values(index+2, 2)
+    table_name = read_sheet.row_values(0, 2)
+    for i in range(len(table_value)):
+        if table_value[i] == '':
+            table_value[i] = 'null'
+    return table_name, table_value
+
+
+def get_by_day(time):              #获取一天的二维数组的数据
+    filename = '三线窑操作记录总表.xlsx'
+    readfile = xlrd.open_workbook(filename)
+    read_sheet = readfile.sheet_by_name('Sheet1')
+    table_data = read_sheet.col_values(0, 1)
+    index = 0
+    for data in table_data:
+        if data == time:
+            index = table_data.index(data)
+            break
+    table_name = read_sheet.row_values(0, 2)
+    table_value = []
+    for i in range(len(table_name)):
+        x = read_sheet.col_values(i+2, index, index+23)
+        for j in range(len(x)):
+            if x[j] == '':
+                x[j] = 'null'
+        table_value.append(x)
+    return table_name, table_value
+
+
+'''
+if __name__ == "__main__":
+    x, y = get_by_day(20170123)
+    print(x)
+    print(y)
+    '''
